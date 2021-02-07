ddf468be1258297654df353dd53bd441c004deda
Enrico Spinielli <enrico.spinielli@gmail.com>
2021-02-07 15:02:27 +0100

    added parsin of CRCO are; regenerated docs

--- R/country_fir.R
+++ R/country_fir.R
@@ -6,6 +6,8 @@
 #'                It can be a regular expression, i.e. "LI|LF" for matching France and Italy.
 #' @param fl      flight level at which assemble the composing polygons.
 #' @param merge   Do you want to merge? (Default: TRUE).
+#' @param exclude List of IDs to filter out. It can be useful to use when only continental
+#'                portion of FIR is of interest, i.e. exclude SANTA MARIA FIR for Portugal.
 #'
 #' @export
 #' @return An Simple Feature for the relevant country FIR.
@@ -15,10 +17,18 @@
 #' country_fir(pruatlas::firs_nm_406, "LF")
 #' # EUROCONTROL's Member States
 #' country_fir(pruatlas::firs_nm_406, icao_id = "E.|L.|UD|UG|GM|UK|GC")
+#' # Consider only continental portion of Portugal FIR
+#' country_fir(pruatlas::firs_nm_406, icao_id = "LP", exclude = c("LPPOFIR"))
 #' }
-country_fir <- function(firs, icao_id = "LI", fl = 0, merge = TRUE) {
+country_fir <- function(firs, icao_id = "LI", fl = 0, merge = TRUE, exclude = NULL) {
   firs %>%
     dplyr::filter(stringr::str_detect(.$icao, icao_id) & .$min_fl <= fl & fl <= .$max_fl) %>%
+    {
+      if (!is.null(exclude)) {
+        dplyr::filter(., !.$id %in% exclude)
+      }
+      else .
+    } %>%
     sf::st_as_sf() %>%
     {
       if (merge) {
---
+++ R/parse_airspace.R
@@ -0,0 +1,88 @@
+#' Parse CRCO Airspace Charging Zone format
+#'
+#' @description
+#'
+#' `parse_airspace_crco` reads the airspace CRCO format and returns a tibble
+#' where each row describes an airspace.
+#' The airspace file can be found on \href{https://www.eurocontrol.int/publication/route-state-overflown-rso-distance-tool-area-boundary-model}{EUROCONTROL website}.
+#'
+#' Each airspace in CRCO format is described by:
+#' \enumerate{
+#'   \item Airspace description (one row):
+#'     \itemize{
+#'       \item `nb_point` the number of points
+#'       \item `latitude` the latitude (of the label)
+#'       \item `longitude` the longitude (of the label)
+#'       \item `flights`
+#'       \item `bottom_level` low level of the volume (flight level)
+#'       \item `top_level` high level of the volume (flight level)
+#'       \item `surface`
+#'       \item `sector_num`
+#'       \item `flight time`
+#'       \item `traffic density`
+#'       \item `x mileage`
+#'       \item `rte extens.`
+#'       \item `value 1`
+#'       \item `value 2`
+#'       \item `name` airspace name/code
+#'     }
+#'   \item `latitude` `longitude`: coordinates (`nb_point` rows in minutes decimal)
+#' }
+#'
+#' @param lines text lines of the PRISME airspace representation
+#'
+#' @return a tibble of airspaces (CRS = 4326)
+#'
+#' @family read/export
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' bo <- system.file("extdata", "sbm_bz_20200527.txt", package = "pruatlas")
+#' crco <- readr::read_lines(bo) %>%
+#'   parse_airspace_crco()
+#' }
+parse_airspace_crco <- function(lines) {
+  airspace <- list()
+  i <- 1
+  pts <- NULL
+
+  for (l in lines) {
+    if (is.null(pts)) {
+      # parse the airspace header
+      h <- stringr::str_split(l, " ") %>% `[[`(1)
+      name <- h[15] # airspace name
+      fl_m <- h[5] %>% as.integer() # min FL
+      fl_M <- h[6] %>% as.integer() # Max FL
+      lon <- (h[3] %>% as.double()) / 60.0
+      lat <- (h[2] %>% as.double()) / 60.0
+      n <- h[1] %>% as.integer() # number of vertices
+      len <- n
+      pts <- character(n)
+    } else {
+      if (n > 0) {
+        pts[len - n + 1] <-  l
+        n <- n - 1
+        if (n == 0) {
+          pol <- pts %>%
+            stringi::stri_split(fixed = " ", simplify = TRUE) %>%
+            apply(2, as.double) %>%
+            magrittr::divide_by(60.0) %>%
+            {list(.)} %>%
+            as.data.frame() %>%
+            sf::st_as_sf(coords = c("X2", "X1"), crs = 4326) %>%
+            dplyr::summarise(unit = name, fl_min = fl_m, fl_max= fl_M,
+                             lon = lon, lat = lat,
+                             geometry = sf::st_combine(.$geometry)) %>%
+            sf::st_cast("POLYGON")
+
+          airspace[[i]] <- pol
+          i <- i + 1
+          pts <- NULL
+        }
+      }
+    }
+  }
+  # sf::st_as_sf(data.table::rbindlist(airspace))
+  sf::st_as_sf(dplyr::bind_rows(airspace))
+}
