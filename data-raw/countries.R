22e2ea595e57062dd83c99be8b678586b447994c
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2025-06-01 16:03:00 +0200

    updated country codes for Kosovo in country data

--- data-raw/countries.R
+++ data-raw/countries.R
@@ -3,7 +3,15 @@ library(rnaturalearth)
 library(dplyr)
 library(sf)
 
-countries50m <- ne_countries(scale = 50, type = "countries", returnclass = "sf") %>%
-  select(iso_a3, iso_n3, admin)
+countries50m <- ne_countries(
+  scale = 50,
+  type = "countries",
+  returnclass = "sf"
+) %>%
+  select(iso_a3, iso_n3, admin) |>
+  mutate(
+    iso_a3 = if_else(admin == "Kosovo", "XKX", iso_a3),
+    iso_n3 = if_else(admin == "Kosovo", "926", iso_n3)
+  )
 
 usethis::use_data(countries50m, overwrite = TRUE)
